f933038e2b9710d7d688f404f7748e55de56ca13
fredye <fredweb3@gmail.com>
2022-01-26 00:52:19 -0300

    Build view cliente, funcionario e veiculo.

--- src/main/java/br/com/loccar/controller/ClienteController.java
+++ src/main/java/br/com/loccar/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.loccar.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,28 +14,28 @@ import br.com.loccar.repositories.ClienteRepository;
 @Controller
 public class ClienteController implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	@Autowired
 	private ClienteRepository repository;
-	
+
 	/**
-	* Redireciona para pagina listar livro
-	**/
-	@RequestMapping("cliente/clientes")
-	public String cadastrar(Model model) {
-		model.addAttribute("clientes", repository.findAll());
+	 * Redireciona para pagina listar clientes
+	 **/
+	@RequestMapping("/clientes")
+	public String listar(Model model) {
+		List<Cliente> clientes = repository.findAll();
+		model.addAttribute("clientes", clientes);
 		return "cliente/clientes";
 	}
 
-	/*
-	 * @RequestMapping("cliente/cadastrar-cliente") public String
-	 * showNewProductPage(Model model) { Cliente cliente = new Cliente();
-	 * model.addAttribute("cliente", cliente);
-	 * 
-	 * return "cliente/cadastrar-cliente"; }
-	 */
-	
-	
+	/**
+	 * Redireciona para pagina salvar cliente
+	 **/
+	@RequestMapping("/cadastrar-cliente")
+	public String navegacaoCadastrar(Model model) {
+		Cliente cliente = new Cliente();
+		model.addAttribute("cliente", cliente);
+		return "cliente/cadastrar-cliente";
+	}
 }
